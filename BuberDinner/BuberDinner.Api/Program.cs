408567b2a3bc93ad798e3192620fd04243ac413d
LMuchapirei <h170407h@hit.ac.zw>
2023-09-17 13:19:19 +0200

    ErrorAttributeFilters Implementation

--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,4 +1,5 @@
-using BuberDinner.Api.MiddleWare;
+using BuberDinner.Api.Filters;
+// using BuberDinner.Api.MiddleWare; import for the middleware
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
 
@@ -7,12 +8,12 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers();
+    builder.Services.AddControllers(options=>options.Filters.Add<ErrorHandlingFilterAttribute>()); // Option 2 to create an error handling filter attribute
 }
 
     var app = builder.Build();
 {
-    app.UseMiddleware<ErrorHandlingMiddleware>();
+    // app.UseMiddleware<ErrorHandlingMiddleware>(); custom implementation registerd here defined in Middeware Option 1 as indicated in the README.md file
     app.MapControllers();
     app.Run();
 }
