1fe843133b559aac1391e3cdfefffdcf19460cb2
LMuchapirei <h170407h@hit.ac.zw>
2023-09-17 14:07:09 +0200

    Defining the custom UseExceptionHanlder

---
+++ BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BuberDinner.Api.Controllers;
+
+
+ [Route("/error")]
+public class ErrorsController: ControllerBase {
+    
+    public IActionResult Error()
+    {
+        Exception? exception  = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
+        return Problem(
+            title:exception?.Message
+        );
+    }
+}
--- BuberDinner/BuberDinner.Api/Program.cs
+++ BuberDinner/BuberDinner.Api/Program.cs
@@ -1,19 +1,28 @@
+using BuberDinner.Api.Errors;
 using BuberDinner.Api.Filters;
 // using BuberDinner.Api.MiddleWare; import for the middleware
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {   
     builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
-    builder.Services.AddControllers(options=>options.Filters.Add<ErrorHandlingFilterAttribute>()); // Option 2 to create an error handling filter attribute
+    // builder.Services.AddControllers(options=>options.Filters.Add<ErrorHandlingFilterAttribute>()); // Option 2 to create an error handling filter attribute
+    builder.Services.AddControllers();
+
+    // Apply custom problem factory
+
+    builder.Services.AddSingleton<ProblemDetailsFactory,BuberDinnerProblemDetailsFactory>();
 }
 
     var app = builder.Build();
 {
     // app.UseMiddleware<ErrorHandlingMiddleware>(); custom implementation registerd here defined in Middeware Option 1 as indicated in the README.md file
+    app.UseExceptionHandler("/error"); 
     app.MapControllers();
     app.Run();
 }
