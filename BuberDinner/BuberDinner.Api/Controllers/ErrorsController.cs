9c45aa231e4bb40d4f515192f8030cc6de7dd6e9
LMuchapirei <h170407h@hit.ac.zw>
2023-09-17 17:19:40 +0200

    Abstract Away the exception in an Interface, then implement different interfaces for each exception type

--- BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
+++ BuberDinner/BuberDinner.Api/Controllers/ErrorsController.cs
@@ -13,7 +13,7 @@ public class ErrorsController: ControllerBase {
         Exception? exception  = HttpContext.Features.Get<IExceptionHandlerFeature>()?.Error;
         var (statusCode,message) = exception switch
         {
-            DuplicateEmailException => (StatusCodes.Status409Conflict,"Email already exists."),
+            IServiceException serviceException => ((int)serviceException.StatusCode,serviceException.ErrorMessage),
             _=> (StatusCodes.Status500InternalServerError,"An unexpected error occurred")
         };
         return Problem(statusCode:statusCode,title:message);
--- BuberDinner/BuberDinner.Application/Common/Errors/DuplicateEmailException.cs
+++ BuberDinner/BuberDinner.Application/Common/Errors/DuplicateEmailException.cs
@@ -1,9 +1,15 @@
+using System.Net;
+
 namespace BuberDinner.Application.Common.Errors;
 
 
-public class DuplicateEmailException: Exception {
+public class DuplicateEmailException: Exception, IServiceException {
     public DuplicateEmailException(string message):base(message)
     {
         
     }
+
+    public HttpStatusCode StatusCode => HttpStatusCode.Conflict;
+
+    public string ErrorMessage => "Email already exists.";
 }
